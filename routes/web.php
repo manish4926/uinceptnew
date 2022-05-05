5b883d67ac2ef540be6cbc6b57fc2f814792c96c
Manish Arora <manish.arora4926@gmail.com>
2022-05-05 17:58:56 +0530

    Template opened

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+//use App\Http\Controllers\MainController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/','MainController@home')->name('home');
+
+
+require __DIR__.'/auth.php';
