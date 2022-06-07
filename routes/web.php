8f725ce81656571a5863fce88a99c25623a68cdf
Manish Arora <manish.arora4926@gmail.com>
2022-06-07 10:23:17 +0530

    updating main branch

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/','MainController@home')->name('home');
 
 require __DIR__.'/auth.php';
